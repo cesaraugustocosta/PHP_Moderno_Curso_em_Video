f91a97f6004a493730e75fd8cc22c3780b0966c6
cesaraugustocosta <cesaraug76@hotmail.com>
2023-03-18 11:20:19 -0300

    Adicionando o phpinfo

--- ex001/index.php
+++ ex001/index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Dados do Servidor</title>
 </head>
 <body>
-   <h1>Olá mundo</h1> 
+<h1>Dados do Servidor</h1>
+<?php
+phpinfo();
+?>
 </body>
 </html>
