eac7b634afff67c40b4a97ebaf1ddc6d6303c034
Cesar Augusto <cesaraug76@hotmail.com>
2023-03-21 20:17:24 -0300

    Tipos Primitivos em PHP

---
+++ ex004/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos Primitivos do PHP</title>
+</head>
+<body>
+    <h1>Teste de Tipo Primitivos do PHP</h1>
+    <?php
+       $num = 0x1A;
+       echo "O valor da váriavel é $num";
+
+       //$v = "Gustavo";
+      // $v = (int) 3e2;
+       //var_dump($v);
+
+       $casado = false;
+       var_dump($casado);
+
+       $vet = [5, 7, 8,10];
+       var_dump($vet);
+    
+    ?>
+</body>
+</html>
